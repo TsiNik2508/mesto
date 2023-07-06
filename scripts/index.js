5a59fe7f441b73880eecb4712cdc88ee4335aa29
Nikita Tsilosani <personalnick@yandex.ru>
2023-07-06 23:28:22 +0300

    revie 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,33 @@
-const editProfileButton = document.querySelector('.profile__edit-button');
-const popup = document.getElementById('user');
-const closeButton = popup.querySelector('.popup__close');
-const formElement = popup.querySelector('.popup__form');
-const saveButton = popup.querySelector('.popup__button');
-const nameInput = formElement.querySelector('.popup__input_type_name');
-const bioInput = formElement.querySelector('.popup__input_type_bio');
-const nameElement = document.querySelector('.profile__title');
-const bioElement = document.querySelector('.profile__bio');
+const editButton = document.querySelector('.profile__edit-button'); 
+const popup = document.querySelector('.popup'); 
+const closeButton = popup.querySelector('.popup__close'); 
+const saveButton = popup.querySelector('.popup__button'); 
+const nameInput = popup.querySelector('.popup__input_type_name'); 
+const bioInput = popup.querySelector('.popup__input_type_bio'); 
+const titleElement = document.querySelector('.profile__title'); 
+const bioElement = document.querySelector('.profile__bio'); 
 
-function openPopup() {
-  popup.style.display = 'flex';
-  setTimeout(() => {
-    popup.style.opacity = '1';
-  }, 0);
-  popup.style.pointerEvents = 'auto'; // Включаем обработку событий на попапе
-}
 
-function closePopup() {
-  popup.style.opacity = '0';
-  popup.style.pointerEvents = 'none';
-}
+function openPopup() { 
+  popup.classList.add('popup_opened'); 
+  nameInput.value = titleElement.textContent; 
+  bioInput.value = bioElement.textContent; 
+} 
+ 
+function closePopup() { 
+  popup.classList.remove('popup_opened'); 
+} 
+ 
+function handleFormSubmit(evt) { 
+  evt.preventDefault(); 
+ 
+  titleElement.textContent = nameInput.value; 
+  bioElement.textContent = bioInput.value; 
+ 
+  closePopup(); 
+} 
+ 
+editButton.addEventListener('click', openPopup); 
+closeButton.addEventListener('click', closePopup); 
+saveButton.addEventListener('click', handleFormSubmit); //если сделать submit, то страница перезагружается и данные не сохранятся
 
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-
-  const newName = nameInput.value;
-  const newBio = bioInput.value;
-
-  nameElement.textContent = newName;
-  bioElement.textContent = newBio;
-
-  closePopup();
-}
-
-editProfileButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
