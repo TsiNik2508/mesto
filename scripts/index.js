cf7babe12b5e38ef447bd2a366866bd3b7ec5dbd
Nikita <personalnick@yandex.ru>
2023-07-25 21:46:32 +0300

    constants.js

--- scripts/index.js
+++ scripts/index.js
@@ -120,34 +120,6 @@ document.addEventListener('DOMContentLoaded', function () {
     closeAddCardPopup();
   });
 
-  // Исходные карточки
-  const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
-
   // Добавление исходных карточек на страницу
   initialCards.forEach((cardData) => {
     addCardToPage(cardData);
